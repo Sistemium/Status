b5c61254b78e25ebc5422b0875b606d1b30540a6
Denis Mosin <deniss.mosin@gmail.com>
2015-07-15 19:02:26 +0300

    handlebars.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,33 +2,46 @@
 'use strict';
 
 $(document).ready(function () {
-  var text ='';
+
+  var labels = [
+    ['Pool', 'pool'],
+    ['Total processes', 'total processes'],
+    ['Total of accepted connections', 'accepted conn'],
+    ['Idle Processes', 'idle processes'],
+    ['Listen Queue', 'listen queue'],
+    ['Listen Queue Length', 'listen queue len'],
+    ['Process Manager', 'process manager'],
+    ['Max Active Processes', 'max active processes'],
+    ['Max Children Reached', 'max children reached'],
+    ['Slow Requests', 'slow requests'],
+    ['Start Since', 'start since'],
+    ['Start Time', 'start time']
+  ];
+
   $.ajax({
   dataType: 'json',
   url: 'https://api.sistemium.com/status?json',
   success: function(jsondata){
-    text += '<ul сlass="sist_list">'
-    text += '<li>Pool = ' + jsondata.pool + '</li>'
-    text += '<li>Total processes = ' + jsondata['total processes'] + '</li>'
-    text += '<li>Total of accepted connections = ' + jsondata['accepted conn'] + '</li>'
-    text += '<li>Idle Processes = ' + jsondata['idle processes'] + '</li>'
-    text += '<li>Listen Queue = ' + jsondata['listen queue'] + '</li>'
-    text += '<li>Listen Queue Length = ' + jsondata['listen queue len'] + '</li>'
-    text += '<li>Process Manager = ' + jsondata['process manager'] + '</li>'
-    text += '<li>Max Active Processes= ' + jsondata['max active processes'] + '</li>'
-    text += '<li>Max Children Reached = ' + jsondata['max children reached'] + '</li>'
-    text += '<li>Slow Requests= ' + jsondata['slow requests'] + '</li>'
-    text += '<li>Start Since = ' + jsondata['start since'] + '</li>'
-    text += '<li>Start Time = ' + jsondata['start time'] + '</li>'
-    text += '<li>Start Time = ' + jsondata['start time'] + '</li>'
-    text += '<li>Total Processes = ' + jsondata['total processes'] + '</li>'
 
+    $.get('templates/stats-aggregated.html', function(template) {
 
-    //$('.jumbotron').html('Pool = ' + jsondata.pool);
-    //$('.jumbotron').append('Total processes = ' + jsondata['total processes']);
-    text += '</ul>'
-    $('.jumbotron').append(text);
-    $('.jumbotron').append('Total number of arguments: ' + Object.keys(jsondata).length);
+      var tpl = Handlebars.compile(template),
+        tplData = {
+          items:[],
+          numberOfProperties: Object.keys(jsondata).length
+        }
+      ;
+
+      labels.forEach (function (label) {
+        tplData.items.push ({
+          label: label[0],
+          value: jsondata[label[1]]
+        })
+      })
+
+      $('.jumbotron').html(tpl(tplData));
+
+    });
 
   }
 });
