368d91bcd3ba7565adf72e4751332b23721fcc6a
Denis Mosin <deniss.mosin@gmail.com>
2015-08-14 15:48:48 +0300

    routing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,7 @@
 // jshint devel:true
 'use strict';
 
+
 $(document).ready(function () {
 
   var labels = [
@@ -19,32 +20,48 @@ $(document).ready(function () {
   ];
 
   $.ajax({
-  dataType: 'json',
-  url: 'https://api.sistemium.com/status?json',
-  success: function(jsondata){
+    dataType: 'json',
+    url: 'https://api.sistemium.com/status?json',
+    success: function(jsondata){
+      $.get('templates/stats-aggregated.html', function(template) {
+        var tpl = Handlebars.compile(template),
+          tplData = {
+            items:[],
+            numberOfProperties: Object.keys(jsondata).length
+          };
 
-    $.get('templates/stats-aggregated.html', function(template) {
-
-      var tpl = Handlebars.compile(template),
-        tplData = {
-          items:[],
-          numberOfProperties: Object.keys(jsondata).length
-        }
-      ;
-
-      labels.forEach (function (label) {
-        tplData.items.push ({
-          label: label[0],
-          value: jsondata[label[1]]
+        labels.forEach (function (label) {
+          tplData.items.push ({
+            label: label[0],
+            value: jsondata[label[1]]
+          })
         })
-      })
 
-      $('.main').html(tpl(tplData));
+        $('.main').html(tpl(tplData));
 
+      });
+    }
+  });
+
+  var setLocation = function setLocation (path) {
+    $('.nav > .button').each (function () {
+      var el = $(this);
+      if (el.find('a').attr('href') == path) {
+        el.addClass('active');
+      } else {
+        el.removeClass('active');
+      }
     });
+    $('.copy').html(path);
+  };
 
-  }
-});
+  $('.button').click(function(){
+    setLocation ($(this).find('a').attr('href'));
+  });
+
+  setLocation (location.hash || '#home');
+
+  $('.not-ready').removeClass('not-ready');
 
 });
 //# sourceMappingURL=main.js.map
