7dbcea59abab15c54c338accf457cebf7526667b
Denis Mosin <deniss.mosin@gmail.com>
2015-08-26 16:24:35 +0300

    handlebars.js created for outputing Start since

---
+++ app/scripts/handlebars.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function (Handlebars) {
+
+  var iifHelper = function (prop,suf) {
+    return function () {
+      return this[prop]() ? this[prop]() + suf : '';
+    }
+  };
+
+  ['days','hours','minutes','seconds'].forEach(function(key){
+    Handlebars.registerHelper (key,iifHelper(key,key.charAt(0)+' '));
+  });
+
+})(Handlebars);
