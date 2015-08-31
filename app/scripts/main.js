425d714494dba37de4b74fb0fbab4df82c28da7d
Denis Mosin <deniss.mosin@gmail.com>
2015-08-31 18:08:58 +0300

    binding data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -56,6 +56,8 @@ $(document).ready(function () {
       });
     }
 
+    $('.outlet.stats-aggregated').html(tpl(tplData));
+    $('.fullStats').html(fullTpl(tplFullData));
   }
 
   function loadStatusTpl(){
